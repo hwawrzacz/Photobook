1e7b600aadd2c45fb73309d90c1617bb49654080
Hubert <hubert.wawrzacz@gmail.com>
2019-11-02 14:48:16 +0100

    Add font-size change to textBox

--- classes/TextBox.js
+++ classes/TextBox.js
@@ -1,34 +1,65 @@
 class TextBox extends Element2D {
     constructor(top = 0, left = 0, width = 300, height = 50, rotation = 0) {
         super();
-        const parser = new DOMParser().parseFromString(this.textBoxPattern,"text/html");
-        this.element = parser.querySelector(".textbox-container");
+        const textBoxDocument = new DOMParser().parseFromString(this.textBoxPattern,"text/html");
+        this.element = textBoxDocument.querySelector(".textbox-container");
         
         this.top = top;
         this.left = left;
         this.width = width;
         this.height = height;
         this.rotation = rotation;
+
+        this.textAlign = "left";
+        this.fontSize = 14;
         
         this.initializeHooksMechanism();
     }
+
+    //region Getters and setters
+    get textAlign() {
+        return this.TextAlignment;
+    }
+
+    set textAlign(value) {
+        this.TextAlign = value;
+        this.element.style.setProperty("text-align", value);
+    }
+
+    get fontSize() {
+        return this.FontSize;
+    }
+
+    set fontSize(value) {
+        if (value > 0) {   
+            this.FontSize = value;
+            this.element.style.setProperty("font-size", value + "px");
+        }
+    }
+    //endregion
 
     initializeHooksMechanism() {
         const hookResize = this.element.querySelector(".hook-resize");
         const hookRotate = this.element.querySelector(".hook-rotate");
         const hookDelete = this.element.querySelector(".hook-delete");
+        const hookFontUp = this.element.querySelector(".font-up");
+        const hookFontDown = this.element.querySelector(".font-down");
+
+        hookFontUp.addEventListener("mousedown", () => {
+            this.fontSize += 2;
+        });
+
+        hookFontDown.addEventListener("click", () => {
+            this.fontSize -= 2;
+        });
 
         move(this);
         resizeElement(this, hookResize);
         rotateElement(this, hookRotate);
-        hookDelete.addEventListener("click", (e) => {
+        hookDelete.addEventListener("click", () => {
             this.element.remove();
             showInfo("Element was removed");
         });
-    }
-
-    initializeAppearanceModifiers() {
-
     }
 
     textBoxPattern = `
@@ -39,8 +70,8 @@ class TextBox extends Element2D {
                 <li><i class="material-icons hook-text-align-center">format_align_left</i></li>
                 <li><i class="material-icons hook-text-align-justify">format_align_justify</i></li>
                 <li><i class="material-icons hook-text-align-right">format_align_right</i></li>
-                <li><i class="material-icons ">text_rotation_angleup</i></li>
-                <li><i class="material-icons ">text_rotation_angledown</i></li>
+                <li><i class="font-up material-icons">text_rotation_angleup</i></li>
+                <li><i class="font-down material-icons">text_rotation_angledown</i></li>
                 <li><input type="color" /></li>
             </ul>
             
