71b0c96206923b820650dac278129372ca031f5b
Hubert <hubert.wawrzacz@gmail.com>
2019-11-02 22:31:05 +0100

    Add backgrounds to ugly af 'repo'

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -3,11 +3,9 @@ const page = photobook.element;
 
 photobook.addPage();
 photobook.addPage();
-photobook.addPage(fabric1Base64);
-
-console.log(photobook.activePage);
+photobook.addPage(imageBase64);
+photobook.getPage(3).backgroundImage = background4;
+//possible background images: fabric(1-4) background(1-4)
 
 photobook.addImageToPage(imageBase64, 3);
 photobook.addTextBoxToPage(3);
-
-console.log(photobook.activePage)
