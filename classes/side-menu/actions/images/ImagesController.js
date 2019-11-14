506daa266db61087791b3f795976529ca0c992de
KrolKamil <kamilkrol1997@wp.pl>
2019-11-14 14:45:16 +0100

    XD

---
+++ classes/side-menu/actions/images/ImagesController.js
@@ -0,0 +1,32 @@
+class ImagesController extends EventEmitter {
+  constructor () {
+    super();
+    this.imagesView = new ImagesView();
+    this.activated = false;
+    this.init();
+  }
+
+  init = () => {
+    this.eventsHandler();
+  }
+
+  eventsHandler = () => {
+    this.imagesView.on('imageClicked', (img) => {
+      this.emit('imageClicked', img);
+    });
+  }
+
+    activate = () => {
+      if (this.activated === false) {
+        this.activated = true;
+        this.imagesView.activate();
+      }
+    }
+
+    disable = () => {
+      if (this.activated === true) {
+        this.activated = false;
+        this.imagesView.disable();
+      }
+    }
+}
