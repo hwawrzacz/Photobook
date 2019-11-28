c74367397bab8aef742f5737cc839c1e7a8fbc04
KrolKamil <kamilkrol1997@wp.pl>
2019-11-28 15:16:36 +0100

    fixed rotate script

--- scripts/rotate.js
+++ scripts/rotate.js
@@ -1,34 +1,34 @@
-//rotate
+// rotate
 const rotateElement = (target, hook) => {
+  const initializeRotating = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    window.addEventListener('mousemove', rotate);
+    window.addEventListener('mouseup', endRotate);
+  };
 
-    const initializeRotating = (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        window.addEventListener('mousemove', rotate);
-        window.addEventListener('mouseup', endRotate);
-    };
+  const main = document.querySelector('#view');
 
-    const rotate = (event) => {
-        let centerX = target.left + (target.width / 2);
-        let centerY = target.top + (target.height / 2);
-        let mouseX = parseFloat(event.pageX);
-        let mouseY = parseFloat(event.pageY);
-        let radians = Math.atan2(mouseX - centerX, mouseY - centerY);
+  const rotate = (event) => {
+    let centerX = target.left + (target.width / 2);
+    let centerY = target.top + (target.height / 2);
+    let mouseX = parseFloat(event.pageX - main.offsetLeft);
+    let mouseY = parseFloat(event.pageY - main.offsetTop);
+    let radians = Math.atan2(mouseX - centerX, mouseY - centerY);
+    let degree = (radians * (180 / Math.PI) * -1);
 
-        let degree = (radians * (180 / Math.PI) * -1);
+    if (event.shiftKey) {
+      degree = Math.round((degree / 15)) * 15;
+    }
 
-        if (event.shiftKey){
-            degree = Math.round((degree/15))*15;
-        }
-        
-        target.rotation = degree;
-        showInfo(getElementProperties(target.element));
-    };
+    target.rotation = degree;
+    showInfo(getElementProperties(target.element));
+  };
 
-    const endRotate = () => {
-        window.removeEventListener('mousemove', rotate);
-        window.removeEventListener('mouseup', endRotate);
-    };
-    
-    hook.addEventListener('mousedown', initializeRotating);
+  const endRotate = () => {
+    window.removeEventListener('mousemove', rotate);
+    window.removeEventListener('mouseup', endRotate);
+  };
+
+  hook.addEventListener('mousedown', initializeRotating);
 };
