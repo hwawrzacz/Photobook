b222a1e89e3e0d31c7288c6ba78fd87c0ae519c8
mostalecki <1b13ostalecki@gmail.com>
2019-12-05 18:01:31 +0100

    Move element rotation to separate method

--- classes/PDFExporter.js
+++ classes/PDFExporter.js
@@ -8,35 +8,24 @@ class PDFExporter {
 
         var widthRatio = doc.internal.pageSize.getWidth()/photobook.width;
         var heightRatio = doc.internal.pageSize.getHeight()/photobook.height;
-
         
+        var pageCount = 1;
         for (let page of photobook.pages) {
             //loop with all pages of photobook
-
+            doc.addImage(page.backgroundImage, "JPEG", 0, 0, page.width, page.height, "background", "NONE");
+            var imgCount = 1;
             for (let image of page.images){
                 //loop with all images of page
 
-                //TODO: put all the rotation logic in other method
-                if (-image.rotation >=0 ){
-                    var angle = -image.rotation;
-                }
-                else{
-                    var angle = 360-image.rotation;
-                } // angle in full 360 degree range
+                var angle = this.getAngle(image.rotation)// angle in full 360 degree range
 
-                var rotation = image.rotation * (Math.PI/180); //rotation in radians
+                var rotatedOrigin = this.getRotatedOrigin(image.left, image.top, image.width, image.height,
+                    widthRatio, heightRatio, image.rotation)
 
-                var centerX = image.left*widthRatio+(image.width*widthRatio)/2;
-                var centerY = image.top*heightRatio+(image.height*heightRatio)/2;
+                doc.addImage(image.backgroundImage, "JPEG", rotatedOrigin.x, rotatedOrigin.y-image.height*heightRatio,
+                image.width*widthRatio, image.height*heightRatio,"img"+pageCount+"."+imgCount, "NONE", angle);
 
-                var bottomX = image.left*widthRatio;
-                var bottomY = image.top*heightRatio+image.height*heightRatio;
-                
-                var newX = centerX + (bottomX-centerX)*Math.cos(rotation) - (bottomY-centerY)*Math.sin(rotation);
-                var newY = centerY + (bottomX-centerX)*Math.sin(rotation) + (bottomY-centerY)*Math.cos(rotation);
-
-                doc.addImage(image.backgroundImage, "JPEG", newX,
-                newY-image.height*heightRatio, image.width*widthRatio, image.height*heightRatio, "XD", "NONE", angle);
+                imgCount++;
             }
             for (let textBox of page.textBoxes){
                 //loop with all textBoxes of page
@@ -58,5 +47,26 @@ class PDFExporter {
         }
         doc.deletePage(2);
         doc.save('elo.pdf');
+    }
+
+    //TODO: Refactor it so that widthRatio and heightRatio are uniform for entire photobook and not passed as parameter
+    getRotatedOrigin(x, y, width, height, widthRatio, heightRatio, rotation) {
+        var rot = rotation * (Math.PI/180); //rotation in radians
+
+        var centerX = x*widthRatio+(width*widthRatio)/2;
+        var centerY = y*heightRatio+(height*heightRatio)/2;
+
+        var bottomX = x*widthRatio;
+        var bottomY = y*heightRatio+height*heightRatio;
+                
+        var newX = centerX + (bottomX-centerX)*Math.cos(rot) - (bottomY-centerY)*Math.sin(rot);
+        var newY = centerY + (bottomX-centerX)*Math.sin(rot) + (bottomY-centerY)*Math.cos(rot);
+
+        return {x:newX, y:newY}
+    }
+
+    getAngle(rotation){
+        if (-rotation >=0 ) return -rotation;
+        return 360-rotation;
     }
 }
