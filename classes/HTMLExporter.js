357917be5fa17a0e3d68adbd0af7f9ec8ce828cc
Hubert Wawrzacz <hubert.wawrzacz@gmail.com>
2020-01-03 18:02:10 +0100

    Fix HTML export

--- classes/HTMLExporter.js
+++ classes/HTMLExporter.js
@@ -51,7 +51,30 @@ class HTMLExporter {
     }
 
     styleTemplate = `
+    *{box-sizing: border-box;}
     body{display:-webkit-box;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center}main#view{display:-webkit-box;display:-ms-flexbox;display:flex;width:100% !important;height:auto !important;-ms-flex-wrap:wrap;flex-wrap:wrap;-webkit-box-pack:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center}div.page{position:relative;min-width:595px;min-height:842px;margin:20px;border:1px solid black;overflow:hidden;visibility:visible !important}div.page:first-child{margin:20px 100%}.image-container{position:absolute;background-position:center;background-size:100% 100%;cursor:default}.textbox-container{position:absolute}.textbox-container input[type=text]{position:relative;top:0;left:0;width:100%;height:100%;padding:10px;font-size:inherit;font-weight:inherit;font-style:inherit;text-decoration:inherit;text-align:inherit;border:none;outline:none}.textbox-tools,.hooks-container{display:none}
+
+    .element-2d-container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 500px;
+        height: 300px;
+        -webkit-transform: rotate(0deg);
+                transform: rotate(0deg); }
+        .element-2d-container .image-container {
+          position: absolute;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          right: 0;
+          width: initial;
+          height: initial;
+          background-position: center;
+          background-size: 100% 100%;
+          background-repeat: no-repeat;
+          border: 1px solid black;
+          cursor: move; }
 
     .textbox-container .textbox, .textbox-container a.url-text {
         display: inline-block;
